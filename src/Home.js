9cfd652e39026decc3d14324ca0d86c8dd97cded
Squid <brewingsquid@gmail.com>
2023-11-21 17:01:36 +0100

    Axio

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = ({ posts }) => {
       {posts.length ? (
         <Feed posts={posts} />
       ) : (
-        <p style={{ marginTop: "2rem" }}></p>
+        <p style={{ marginTop: "2rem" }}>No posts to display.</p>
       )}
     </main>
   );
